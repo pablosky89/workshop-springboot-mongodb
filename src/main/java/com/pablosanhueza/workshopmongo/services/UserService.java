a8ff207c5db2168a8eb95637d8f5a52b18fc0dbb
Pablo Sanhueza <pablo.sanhueza.fontalba@gmail.com>
2020-12-12 13:46:30 -0300

    Inserção de usuário com POST

--- src/main/java/com/pablosanhueza/workshopmongo/services/UserService.java
+++ src/main/java/com/pablosanhueza/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.pablosanhueza.workshopmongo.domain.User;
+import com.pablosanhueza.workshopmongo.dto.UserDTO;
 import com.pablosanhueza.workshopmongo.repositories.UserRepository;
 import com.pablosanhueza.workshopmongo.services.exceptions.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> user = repo.findById(id);
 		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto no encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(),objDto.getName(), objDto.getEmail());
 	}
 }
